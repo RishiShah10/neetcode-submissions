35ba69c692a60cdd82f375230153f77a11824b39
RishiShah10 <110631037+RishiShah10@users.noreply.github.com>
2026-05-06 20:52:52 -0700

    Add: merge-intervals - submission-4

---
+++ Data Structures & Algorithms/merge-intervals/submission-4.py
@@ -0,0 +1,19 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        """
+        sort by first value
+        l,r pointers going through list if l[1] > r[0] merge l to be l[0],r[1] and then move r up etc
+        return len of intervals then
+        """
+        intervals.sort(key=lambda x: x[0])
+        l,r = 0,1
+        result = []
+        while r <= len(intervals):
+            currentMax = intervals[l][1]
+            while r < len(intervals) and currentMax >= intervals[r][0]:
+                currentMax = max(currentMax,intervals[r][1])
+                r += 1
+            result.append((intervals[l][0],currentMax))
+            l = r
+            r += 1
+        return result
